24a82cc13639614417ab1f2efb497af1d914c2b5
Panji Kusuma <rr6522et@gmail.com>
2022-08-10 10:53:11 +0700

    fix

--- Handling/DistubeEvent.js
+++ Handling/DistubeEvent.js
@@ -1,6 +1,72 @@
 const { readdirSync } = require('fs');
+const DisTube = require('distube');
+const { SpotifyPlugin } = require("@distube/spotify");
+const { SoundCloudPlugin } = require("@distube/soundcloud");
+const { YtDlpPlugin } = require("@distube/yt-dlp");
+const config = require(`${process.cwd()}/config.json`);
 
 module.exports = async (client) => {
+
+    let plugins;
+    if (config.clientId && config.clientSecret) {
+        plugins = [
+            new SpotifyPlugin({ 
+                parallel: true, 
+                emitEventsAfterFetching: true,
+                api: { clientId: config.clientId, clientSecret: config.clientSecret }
+            }),
+            new SoundCloudPlugin(),
+            new YtDlpPlugin({ update: true })
+        ]
+    } else {
+        plugins = [
+            new SoundCloudPlugin(),
+            new YtDlpPlugin({ update: true })
+        ]
+    }
+
+    let youtubeCookie;
+    if (config.youtubeCookie) {
+        youtubeCookie = config.youtubeCookie;
+    } else {
+        youtubeCookie = 'none';
+    }
+
+    let musicimg = "https://cdn.jsdelivr.net/gh/skick1234/MaBu-CDN@4.8.4/DisTube/img/banner.png";
+    if (config.MusicImg) {
+        client.musicimg = config.MusicImg;
+    } else {
+        client.musicimg = musicimg;
+    }
+
+    const distube = new DisTube.DisTube(client, {
+        searchSongs: 0,
+        searchCooldown: 30,
+        leaveOnEmpty: true,
+        emptyCooldown: 25,
+        leaveOnFinish: false,
+        leaveOnStop: false,
+        nsfw: true,
+        savePreviousSongs: true,
+        plugins: plugins,
+        youtubeCookie: youtubeCookie,
+        ytdlOptions: {
+            highWaterMark: 1024 * 1024 * 64,
+            quality: "highestaudio",
+            format: "audioonly",
+            liveBuffer: 60000,
+            dlChunkSize: 1024 * 1024 * 4,
+        },
+        emitAddListWhenCreatingQueue: true,
+        emitAddSongWhenCreatingQueue: false,
+        emitNewSongOnly: true,
+        joinNewVoiceChannel: true,
+        streamType: 0,
+        directLink: true
+    });
+
+    client.distube = distube;
+
     readdirSync(`${process.cwd()}/Events/Distube/`).forEach(file => {
         const event = require(`${process.cwd()}/Events/Distube/${file}`);
         let eventName = file.split(".")[0];
--- index.js
+++ index.js
@@ -2,10 +2,6 @@
 const { Client, Partials, IntentsBitField } = require("discord.js");
 const { readdirSync } = require("fs");
 const config = require("./config.json");
-const DisTube = require('distube');
-const { SpotifyPlugin } = require("@distube/spotify");
-const { SoundCloudPlugin } = require("@distube/soundcloud");
-const { YtDlpPlugin } = require("@distube/yt-dlp");
 
 const client = new Client({
     allowedMentions: { parse: ['users', 'roles'], repliedUser: true },
@@ -14,69 +10,9 @@ const client = new Client({
     shards: 'auto'
 });
 
-let plugins;
-if (config.clientId && config.clientSecret) {
-    plugins = [
-        new SpotifyPlugin({ 
-            parallel: true, 
-            emitEventsAfterFetching: true,
-            api: { clientId: config.clientId, clientSecret: config.clientSecret }
-        }),
-        new SoundCloudPlugin(),
-        new YtDlpPlugin({ update: true })
-    ]
-} else {
-    plugins = [
-        new SoundCloudPlugin(),
-        new YtDlpPlugin({ update: true })
-    ]
-}
-
-let youtubeCookie;
-if (config.youtubeCookie) {
-    youtubeCookie = config.youtubeCookie;
-} else {
-    youtubeCookie = 'none';
-}
-
-let musicimg = "https://cdn.jsdelivr.net/gh/skick1234/MaBu-CDN@4.8.4/DisTube/img/banner.png";
-if (config.MusicImg) {
-    client.musicimg = config.MusicImg;
-} else {
-    client.musicimg = musicimg;
-}
-
-const distube = new DisTube.DisTube(client, {
-	searchSongs: 0,
-	searchCooldown: 30,
-	leaveOnEmpty: true,
-	emptyCooldown: 25,
-	leaveOnFinish: false,
-	leaveOnStop: false,
-	nsfw: true,
-    savePreviousSongs: true,
-	plugins: plugins,
-    youtubeCookie: youtubeCookie,
-    ytdlOptions: {
-        highWaterMark: 1024 * 1024 * 64,
-        quality: "highestaudio",
-        format: "audioonly",
-        liveBuffer: 60000,
-        dlChunkSize: 1024 * 1024 * 4,
-    },
-    emitAddListWhenCreatingQueue: true,
-    emitAddSongWhenCreatingQueue: false,
-    emitNewSongOnly: true,
-    joinNewVoiceChannel: true,
-    streamType: 0,
-    directLink: true
-});
-
-client.distube = distube;
 client.prefix = config.Prefix;
 client.owner = config.OwnerId;
 client.color = config.Color;
-
 client.logger = require('./Utils/logger');
 
 // Utils Handling
